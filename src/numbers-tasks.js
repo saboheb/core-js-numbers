10f6cd440ddd9a3ba54d316479dbb3ab3e3c2a55
saboheb <100vbo21@gmail.com>
2023-11-13 16:42:45 +0400

    feat: resolutin angel in redian

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -106,8 +106,18 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const chislitelAB = x1 * x2 + y1 * y2;
+  const znamen1 = (x1**2 + y1**2)**0.5;
+  const znamen2 = (x2**2 + y2**2)**0.5;
+  const result =  chislitelAB / znamen1 * znamen2;
+  if (result === 0) {
+    return 'π/2';
+  } else if (result === -1) {
+    return 'π';
+  } else if (result === 1) {
+    return 0;
+  }
 }
 
 /**
