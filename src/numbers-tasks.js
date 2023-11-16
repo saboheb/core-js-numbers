e5ca3b92e88da1c12eca89b00e5f5060ac05adb8
Alex Stolbov <100lbov21@gmail.com>
2023-11-16 22:09:17 +0400

    fix: finish 14th task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -260,8 +260,18 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) {
+    return 0;
+  }
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= index; i += 1) {
+    const c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 }
 
 /**
